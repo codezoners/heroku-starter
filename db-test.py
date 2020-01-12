f6aecd05e644711a27a1bf000a6cfac6056e31b2
Nick (Git client) <nick@cassiel.com>
2020-01-12 13:03:55 +0000

    MongoDB Atlas connection.

--- db-test.py
+++ db-test.py
@@ -1,6 +1,19 @@
 import pymongo
+import os
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# Local database:
+#CONNECTION="mongodb://localhost:27017/"
+
+# MongoDB Atlas:
+user = os.environ["DB_USER"]
+password = os.environ["DB_PASS"]
+
+CONNECTION=\
+    "mongodb+srv://%s:%s@cluster0-ay0js.mongodb.net/test?retryWrites=true&w=majority" \
+    % (user, password)
+
+myclient = pymongo.MongoClient(CONNECTION)
+
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 mydict = { "name": "Jim", "address": "Highway 999" }
--- main.py
+++ main.py
@@ -6,7 +6,18 @@ import pymongo
 
 ON_HEROKU = "ON_HEROKU" in os.environ
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+user = os.environ["DB_USER"]
+password = os.environ["DB_PASS"]
+
+# Local database:
+#CONNECTION="mongodb://localhost:27017/"
+
+# MongoDB Atlas:
+CONNECTION=\
+    "mongodb+srv://%s:%s@cluster0-ay0js.mongodb.net/test?retryWrites=true&w=majority" \
+    % (user, password)
+
+myclient = pymongo.MongoClient(CONNECTION)
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 
