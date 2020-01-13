d01f31801ca0670ca7e8cbdfe310049e1798e8d1
Nick Rothwell <NRothwell@c02stemsgtfl.client.rave.ac.uk>
2020-01-13 16:42:28 +0000

    Pymongo tweaks.

--- db-test.py
+++ db-test.py
@@ -1,6 +1,7 @@
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+#myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+myclient = pymongo.MongoClient("mongodb://tfmp_user:Freedom34@ds263248.mlab.com:63248/tfmp?retryWrites=false")
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 mydict = { "name": "Jim", "address": "Highway 999" }
